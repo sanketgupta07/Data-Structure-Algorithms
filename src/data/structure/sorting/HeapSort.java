39d52fc34ba17182399cc8522250010eae1d4857
Sanket Gupta <sanketgupta07@gmail.com>
2019-08-07 01:16:58 +0530

    Added Heap sort

---
+++ src/data/structure/sorting/HeapSort.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package data.structure.sorting;
+
+/**
+ * @author sanket
+ *
+ */
+public class HeapSort {
+
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int [] input = {7,8,1,9,4,2,6,5}; 
+		HeapSort.sort(input);
+		for (int i : input) {
+			System.out.print(i+" ");
+		}
+	}
+
+	private static void sort(int[] arr) {
+		int n = arr.length;
+		//Convert array to MaxHeap
+		for (int i = n / 2 - 1; i >= 0; i--) 
+			heapify(arr, n, i);
+		for (int i=n-1; i>=0; i--) 
+		{ 
+			// Move current root to end 
+			int temp = arr[0]; 
+			arr[0] = arr[i]; 
+			arr[i] = temp; 
+
+			// call max heapify on the reduced heap 
+			heapify(arr, i, 0); 
+		}
+
+	}
+
+	private static void heapify(int[] arr, int n, int i) {
+		int largest = i;
+		int left = 2*i;
+		int right = 2*i +1;
+
+		if(left < n && arr[left]>arr[largest])
+			largest = left;
+
+		if(right < n && arr[right]>arr[largest])
+			largest = right;
+
+		if(largest != i) {
+			int temp = arr[largest];
+			arr[largest] = arr[i];
+			arr[i]= temp;
+			heapify(arr, n , largest);
+		}
+
+	}
+
+}
