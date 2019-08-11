d382b152a349774eb8979a322f9df06e8c440f98
Sanket Gupta <sanketgupta07@gmail.com>
2019-08-12 01:13:45 +0530

    update

---
+++ src/data/structure/heap/PriorityQueue.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package data.structure.heap;
+
+/**
+ * @author sanket
+ *
+ */
+public class PriorityQueue {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
